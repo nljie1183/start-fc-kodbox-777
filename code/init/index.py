7666007bb5d334d26bd80f570ed9a09c46e360dd
nljie1183 <59471244+nljie1183@users.noreply.github.com>
2025-12-06 02:29:40 +0800

    Update index.py

--- code/init/index.py
+++ code/init/index.py
@@ -5,7 +5,7 @@ import os
 def handler(event, context):
     if not os.path.exists("/mnt/auto/.kodbox-1.35.031"):
         os.system(
-            "wget http://images.devsapp.cn/application/kodbox/kodbox-1.35.031.zip -O /mnt/auto/kodbox-1.35.031.zip")
+            "wget https://static.kodcloud.com/update/download/kodbox.1.64.zip -O /mnt/auto/kodbox-1.35.031.zip")
         os.system(
             "cd /mnt/auto && unzip kodbox-1.35.031.zip && mv kodbox-1.35.031 .kodbox-1.35.031 && rm kodbox-1.35.031.zip && cd -")
     return "nas init"
